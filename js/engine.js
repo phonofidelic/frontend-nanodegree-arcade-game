cc30a24edb732f74850a5193bcc74f954732ef01
phonofidelic <chris_clemons85@hotmail.com>
2015-02-28 05:35:22 -0500

    Added some more characters.

--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,10 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/enemy-bug-left.png',
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png'
     ]);
     Resources.onReady(init);
 
